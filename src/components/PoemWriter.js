0ed3d2767480cffa217860ae1d6c17cab397cdbd
Jon <jbvaldesuso@gmail.com>
2018-05-20 18:01:59 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,50 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      text: "",
+      isValid: true
+    };
+  }
+
+  error = () => {
+    if(!this.state.isValid){
+      return (
+        <div id="poem-validation-error" style={{ color: "red" }}>
+          This poem is not written in the right structure!
+        </div>
+      )
+    }
+  }
+  isValid = (text) => {
+    const poem = text.split("\n").map(p => p.trim())
+    if (poem.length === 3){
+      const line1 = poem[0].split(" ").length === 5;
+      const line2 = poem[1].split(" ").length === 3;
+      const line3 = poem[2].split(" ").length === 5;
+      if(line1 && line2 && line3){
+        return true
+      }
+    }
+    return false
+  }
+  handleText = (e) => {
+
+    this.setState({
+      text: e.target.value,
+      isValid: this.isValid(e.target.value)
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea
+          rows="3"
+          cols="60"
+          value={this.state.text}
+          onChange={this.handleText}/>
+        {this.error()}
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,22 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
 
+  handleMessage = (e) => {
+    this.setState({
+      message: e.target.value
+    })
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input  type="text" value={this.state.message} onChange={this.handleMessage}/>
+        <h4>{this.props.maxChars - this.state.message.length}</h4>
       </div>
     );
   }
